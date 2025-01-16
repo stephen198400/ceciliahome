f3904a6f8a99477de09bc6737b65995f98746743
Cicada <89372746+stephen198400@users.noreply.github.com>
2025-01-16 11:09:34 +0800

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,31 +18,6 @@ const nextConfig = {
 	experimental: {
 		optimizeCss: true,
 	},
-	async headers() {
-		return [
-			{
-				source: '/:path*',
-				headers: [
-					{
-						key: 'Strict-Transport-Security',
-						value: 'max-age=31536000; includeSubDomains',
-					},
-					{
-						key: 'X-Frame-Options',
-						value: 'SAMEORIGIN',
-					},
-					{
-						key: 'X-Content-Type-Options',
-						value: 'nosniff',
-					},
-					{
-						key: 'Referrer-Policy',
-						value: 'strict-origin-when-cross-origin',
-					},
-				],
-			},
-		];
-	},
 };
 
 module.exports = nextConfig;
